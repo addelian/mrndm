2d4d183bfc5e32b3d43de5d72adb05e9eae8846d
Nic <addelia.nicolas@gmail.com>
2025-11-10 21:27:56 -0600

    user,pass only used during initial authentication, replaces with token auth from that point forward

--- mrndm.sh
+++ mrndm.sh
@@ -11,7 +11,7 @@ authbody=$(jq --null-input \
 
 retrieve_memos() {
     if [[ -n "$token" ]]; then
-        curl -u "$username":"$password" -H "Authorization: Token $token" $baseApiUrl/memos/
+        curl -H "Authorization: Token $token" $baseApiUrl/memos/
         exit 0
     fi
     authenticate
@@ -20,7 +20,7 @@ retrieve_memos() {
 
 retrieve_memo() {
     if [[ -n "$token" ]]; then
-        curl -u "$username":"$password" -H "Authorization: Token $token" $baseApiUrl/memos/$1/
+        curl -H "Authorization: Token $token" $baseApiUrl/memos/$1/
         exit 0
     fi
     authenticate
@@ -28,10 +28,12 @@ retrieve_memo() {
 }
 
 authenticate() {
+    echo "token not present in .config file. attempting to generate..."
     tokenjson=$(curl -X POST -H "Content-Type:application/json" -d "$authbody" "$baseApiUrl/auth/")
     token=$(echo $tokenjson | jq -r '.token')
     echo " " >> ./mrndm.config
     echo "token=$token" >> ./mrndm.config
+    echo "token successfully generated and added to config file"
 }
 
 if [ "$1" = "view" ] && [[ $2 =~ ^[0-9]+$ ]]; then
@@ -45,6 +47,24 @@ if [ "$1" = "auth" ]; then
         exit 0
     fi
     authenticate
+    exit 0
+fi
+
+if [ "$1" = "init" ]; then
+    echo "Creating a new user. Enter a username:"
+    read newuser
+    echo "Enter a password:"
+    read -s newpass
+    echo "Re-enter your password:"
+    read -s newpass2
+    registerbody=$(jq --null-input \
+    --arg user "$newuser" \
+    --arg pass "$newpass" \
+    --arg pass2 "$newpass2" \
+    '{username: $user, password: $pass, password2: $pass2}')
+    curl -X POST -H "Content-Type:application/json" -d "$registerbody" "http://127.0.0.1:8000/register/"
+    printf "username=$newuser\npassword=$newpass\nbaseApiUrl=http://127.0.0.1:8000/" > mrndm.config
+    exit 0
 fi
 
 retrieve_memos
